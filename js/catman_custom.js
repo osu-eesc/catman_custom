f0c1e8d925abb25643663ad8c128f3416f8f5a70
Bryan Mayjor <bmayjor@gmail.com>
2014-04-18 16:54:00 -0700

    7-x.1.10 updates. add js spinner catman image

---
+++ js/catman_custom.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+  // Add a spinner on quantity widget.
+  //Drupal.behaviors.quantityWidgetSpinner = {
+    Drupal.behaviors.doug_fir_catalog = {
+    attach: function ( context, settings ) {
+      $('.form-item-quantity input').spinner({
+        min: 1,
+        max: 9999,
+        increment: 'fast'
+      });
+    }
+  }
+})(jQuery);
+
