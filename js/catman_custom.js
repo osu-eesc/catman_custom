b5c306bbea5d744a8a858dfa5b9f5671ef249ede
Bryan Mayjor <bmayjor@gmail.com>
2015-05-28 11:01:31 -0700

    7.x-1.28 - added js for magic link ga send function

--- js/catman_custom.js
+++ js/catman_custom.js
@@ -12,3 +12,27 @@
     }
   }
 })(jQuery);
+
+$('a.magic-link-pdf').on('click', function() {
+  var str = window.location.pathname;
+  var magicLink = str.replace('/viewfile', '');
+  ga('send', 'event', 'Downloads', 'action', {'pdf': '/files/project/pdf/' + magicLink + '.pdf'});
+});
+
+$('a.magic-link-epub').on('click', function() {
+  var str = window.location.pathname;
+  var magicLink = str.replace('/epub', '');
+  ga('send', 'event', 'Downloads', 'action', {'epub': '/files/project/epub/' + magicLink + '.epub'});
+});
+
+$('a.magic-link-preview').on('click', function() {
+  var str = window.location.pathname;
+  var magicLink = str.replace('/preview', '');
+  ga('send', 'event', 'Downloads', 'action', {'pdf': '/files/project/preview' + magicLink + '.pdf'});
+});
+
+$('a.magic-link-excel').on('click', function() {
+  var str = window.location.pathname;
+  var magicLink = str.replace('/excel', '');
+  ga('send', 'event', 'Downloads', 'action', {'excel': '/files/project/' + magicLink + '.xlsx'});
+});
